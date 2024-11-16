9c9fe0acf2dc88562fdb951b3b8ad84a915d7426
nico <nicolasalfonsoferreira@gmail.com>
2024-11-16 11:22:09 -0300

    ch6

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,8 +1,10 @@
 
-from django.contrib import admin
+from django.contrib import admin, auth
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path("accounts/", include("accounts.urls")),
 ]
