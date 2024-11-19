0c9277f94e1aa980c604a1efbc3dc5f1edde092b
nico <nicolasalfonsoferreira@gmail.com>
2024-11-19 17:37:34 -0300

    ch.11

--- books/models.py
+++ books/models.py
@@ -1,4 +1,5 @@
 import uuid
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
 
@@ -14,7 +15,18 @@ class Book(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
-        return self.title
+        return self.title[:125]
     
     def get_absolute_url(self): # new
         return reverse("book_detail", args=[str(self.id)])
+    
+class Review(models.Model):
+
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="reviews")
+    review = models.CharField(max_length=500)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.review[:125]
+    
+
