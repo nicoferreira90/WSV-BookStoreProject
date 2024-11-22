968985f2a9ef1b64b04fa5e11ca32c0f11911132
nico <nicolasalfonsoferreira@gmail.com>
2024-11-22 12:21:00 -0300

    deployment ready

--- django_project/settings.py
+++ django_project/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG", default=False)
 
-ALLOWED_HOSTS = [".fly.dev", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["calm-depths-44080.herokuapp.com", "localhost", "127.0.0.1"]
 
 CSRF_TRUSTED_ORIGINS = ['https://*.fly.dev']
 
